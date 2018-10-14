549c752b7cd2f0e277ea2fe729282ba881852c70
yves <magneticchen@gmail.com>
2018-10-14 23:33:54 +0800

    update

--- src/App.js
+++ src/App.js
@@ -5,13 +5,19 @@ import cover from './imgs/cover.png';
 import cover2 from './imgs/cover2.jpg';
 import mixer from './imgs/mixer.jpg';
 import people from './imgs/people.jpg';
+import program from './imgs/program.jpg';
 import './App.css';
 
 class Header extends Component {
   renderURLs = () => {
     let pages = this.props.header_urls;
     return pages.map(page=> {
-      return <a href={page[1]} key={page[1]}><h2 className="Header-link">{page[0]}</h2></a>
+      if(!page[1].split('://')[1]) {
+        return <a onClick={()=>(this.props.history.push(page[1]))} key={page[1]}><h2 className="Header-link">{page[0]}</h2></a>
+      }
+      else {
+        return <a href={page[1]} key={page[1]}><h2 className="Header-link">{page[0]}</h2></a>
+      }
     });
   }
 
@@ -56,7 +62,7 @@ class Footer extends Component {
           {this.renderLinks()}
         </div>
         <div className="FooterCopyright">
-            copyright©2018 gotoandPlay, 交大網路電台.<br/> 保留一切權利.
+            copyright©2018 goto&Play, 交大網路電台.<br/> 保留一切權利.
         </div>
       </footer>
     )
@@ -125,14 +131,14 @@ class Home extends Component {
     return (
       <div>
         <TextBlock
-          link="/about"
-          title="We are still building our site."
-          contain="This site is still under construction. In order to provide better experience. Please be patient and wait for our update. :)"
-          background="https://78.media.tumblr.com/793329ad53c0c6527a75da62435137cc/tumblr_pdoabfMWHI1uwz0gbo1_1280.gif" height="100px"
+          link={program}
+          title="107上學期節目單"
+          contain="精彩節目不錯過！點我看詳細"
+          background={program}
           height="500px"
         />
-        <TextBlock link="/about" right={true} title="電台一家親，強檔上映" contain="內容" background={people} height="100px"/>
-        <TextBlock title="酷炫mixer，等你來摸"  contain="內容" background={mixer} height="500px"/>
+        <TextBlock link="/about" right={true} title="電台一家親，強檔上映" contain="維持電臺運作的幹部們" background={people} height="100px"/>
+        <TextBlock title="關於電臺"  contain="交大網路電台Goto&Play自開播以來，就一直是系上熱門的話題組織，直到今日電台組織架構及人數持續增長茁壯，相信未來交大網路電台Goto&Play將會持續為聽眾獻上最不一樣的好聲音。" background={mixer} height="500px"/>
       </div>
     )
   }
@@ -168,29 +174,29 @@ class Library extends Component {
 
 class App extends Component {
   state = {
-    title: "gotoandPlay 交大網路電台",
+    title: "goto&Play 交大網路電台",
     pages: [
       ['首頁', '/'],
       ['隨選即播', '/library'],
-      ['臉書', 'https://www.facebook.com/gotoandplay.nctu'],
+      ['臉書', 'https://www.facebook.com/gotoandplay.nctu/'],
       ['關於', '/about']
     ],
     audio_display: "交大網路電台",
     audio_source: "http://140.113.52.126:8000/;",
     footer_links: {
       '深入了解':[
-        ['gotoandPlay?', '/'],
+        ['goto&Play?', '/'],
         ['在tunein收聽', '/'],
 
       ],
       '關於我們':[
         ['所有成員', '/'],
         ['網頁人員', 'https://nooxy.org'],
-        ['網頁開源原始碼', 'https://github.com/magneticchen/gotoandPlay-reactjs']
+        ['網頁開源原始碼', 'https://github.com/magneticchen/goto&Play-reactjs']
       ],
       '社交連結':[
         ['推特', 'https://nooxy.org'],
-        ['臉書', 'https://www.facebook.com/gotoandplay.nctu'],
+        ['臉書', 'https://www.facebook.com/goto&play.nctu'],
         ['tunein', '/']
       ]
     }
@@ -199,16 +205,22 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header title={this.state.title} header_urls={this.state.pages}/>
-        <HorizonPlayer display={this.state.audio_display} source={this.state.audio_source}/>
         <Router>
-          <div>
-            <Route exact path='/' component={Home} />
-            <Route path='/library/' component={Library} />
-            <Route path='/about/' component={About} />
-          </div>
+        <Route exact path=":p(.*)" render={(props)=>{
+          return(
+            <div>
+            <Header history={props.history} title={this.state.title} header_urls={this.state.pages}/>
+            <HorizonPlayer display={this.state.audio_display} source={this.state.audio_source}/>
+              <div>
+                <Route exact path='/' component={Home} />
+                <Route path='/library/' component={Library} />
+                <Route path='/about/' component={About} />
+              </div>
+            <Footer links={this.state.footer_links}/>
+            </div>
+          );
+        }}/>
         </Router>
-        <Footer links={this.state.footer_links}/>
       </div>
     );
   }
