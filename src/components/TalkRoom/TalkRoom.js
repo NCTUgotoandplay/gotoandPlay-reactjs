85d1d805b661728297994e353c3be2a67da1417f
BuildTools <magneticchen@gmail.com>
2019-04-15 20:48:34 +0800

    fix chat overlay

--- src/components/TalkRoom/TalkRoom.js
+++ src/components/TalkRoom/TalkRoom.js
@@ -2,19 +2,22 @@ import React, { Component } from 'react';
 
 class TalkRoom extends Component {
   render() {
-    return (
-      <div className="chat">
-        <div className="chat_room cr2">
-          <div className="chat_icon"><i className="material-icons" id="chat_icon">chat</i></div>
-        </div>
-        <div className="chat_room cr1">
-          <div className="head">
-            <div id="open_chat"></div><i className="material-icons close" id="close_chat">clear</i>
-          </div>
-          <iframe title="nxtalk" className="board" src="https://talk.nooxy.org/noservice/signin"></iframe>
-        </div>
-      </div>
-    );
+    // return (
+    //   <div className="chat">
+    //     <div className="chat_room cr2">
+    //       <div className="chat_icon"><i className="material-icons" id="chat_icon">chat</i></div>
+    //     </div>
+    //     <div className="chat_room cr1">
+    //       <div className="head">
+    //         <div id="open_chat"></div><i className="material-icons close" id="close_chat">clear</i>
+    //       </div>
+    //       <iframe title="nxtalk" className="board" src="https://talk.nooxy.org/noservice/signin"></iframe>
+    //     </div>
+    //   </div>
+    // );
+
+    // chat cause overlay issues 
+    return(<div></div>)
   }
 }
 
