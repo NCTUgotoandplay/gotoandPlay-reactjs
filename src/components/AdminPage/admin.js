7769108d50aad1c3bd743e417b9c038b62f4f922
yves <magneticchen@gmail.com>
2019-10-31 22:33:06 +0800

    applied theme to admin page

--- src/components/AdminPage/admin.js
+++ src/components/AdminPage/admin.js
@@ -9,6 +9,7 @@ import TimetableTableSettings from './Settings/TimetableTable';
 import AlbumsSettings from './Settings/Albums';
 import NoServiceSettings from './Settings/NoService';
 
+
 class AdminPage extends React.Component {
   constructor(props) {
     super(props);
@@ -18,9 +19,9 @@ class AdminPage extends React.Component {
   }
   render () {
     return (
-      <div className="adminpage">
+      <div className="adminpage" style={{'color': 'white', 'background': 'repeating-linear-gradient(45deg, rgba(35, 47, 52, 0.8), rgba(35, 47, 52, 0.8) 10px, rgba(47, 55, 58, 0.8) 10px, rgba(47, 55, 58, 0.8) 20px)'}}>
         <h1>{this.props.localize.admin_page}</h1>
-        <hr />
+        <hr style={{'border': '1px solid'}}/>
         <StatusSettings {...this.props} expanded={this.state.expanded === 'status'} onChange={()=>{this.setState({expanded: this.state.expanded==='status'?null:'status'})}}/>
 
         <NotificationSettings {...this.props} expanded={this.state.expanded === 'notification'} onChange={()=>{this.setState({expanded: this.state.expanded==='notification'?null:'notification'})}}/>
@@ -28,7 +29,7 @@ class AdminPage extends React.Component {
         <ChatRoomSettings {...this.props} expanded={this.state.expanded === 'chatroom'} onChange={()=>{this.setState({expanded: this.state.expanded==='chatroom'?null:'chatroom'})}}/>
 
         <AboutUsSettings {...this.props} expanded={this.state.expanded === 'aboutus'} onChange={()=>{this.setState({expanded: this.state.expanded==='aboutus'?null:'aboutus'})}}/>
-        
+
         <InfoCards {...this.props} expanded={this.state.expanded === 'cards'} onChange={()=>{this.setState({expanded: this.state.expanded==='cards'?null:'cards'})}}/>
 
         <TimetableTableSettings {...this.props} expanded={this.state.expanded === 'TimetableTable'} onChange={()=>{this.setState({expanded: this.state.expanded==='TimetableTable'?null:'TimetableTable'})}}/>
