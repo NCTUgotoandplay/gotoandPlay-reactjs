cee1f3ab254927a8907476be27a9ab2d9893a717
yves <magneticchen@gmail.com>
2019-11-02 19:10:59 +0800

    polished footer

--- src/components/overlays/Footer.js
+++ src/components/overlays/Footer.js
@@ -1,13 +1,20 @@
 import React from "react"
+import Logo from "../../imgs/cover.png"
 
 const Footer = (props) => (
   <div className="footer">
     <div className="container">
       <div className="footer-information">
+        <img className="logo" src={Logo}/>
         <h2>goto&Play</h2>
         <p>We play, we work, we create.</p>
+        <p>Email: gotoandplaynctu@gmail.com</p>
       </div>
-      <p className="copyright"> {props.localize.copyright} </p>
+      <p className="copyright">
+        <span>{props.localize.copyright}</span>
+        <br/>
+        <a href="https://nooxy.org" target="_blank"><span>{'2019- site powered by NOOXY NoService.'}</span></a>
+      </p>
     </div>
   </div>
 )
